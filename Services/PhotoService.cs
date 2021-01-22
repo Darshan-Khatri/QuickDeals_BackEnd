abaa20cf83d50d13a732f573ec7ee06ffa2040d5
Darshan-Khatri <darshankhatri2297@gmail.com>
2021-01-22 00:25:40 -0600

    Cloudinary service is added to store photo

---
+++ Services/PhotoService.cs
@@ -0,0 +1,53 @@
+﻿using CloudinaryDotNet;
+using CloudinaryDotNet.Actions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Options;
+using QuickDeals.Core.IRepositories;
+using QuickDeals.Helper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuickDeals.Services
+{
+    public class PhotoService : IPhotoService
+    {
+        private readonly Cloudinary cloudinary;
+
+        public PhotoService(IOptions<CloudinarySettings> config)
+        {
+            var acc = new Account
+                (
+                    config.Value.CloudName,
+                    config.Value.ApiKey,
+                    config.Value.ApiSecret
+                );
+            cloudinary = new Cloudinary(acc);
+        }
+
+        public async Task<ImageUploadResult> AddPhotoAsync(IFormFile file)
+        {
+            var uploadResult = new ImageUploadResult();
+
+            if (file.Length > 0)
+            {
+                using var stream = file.OpenReadStream();
+
+                var uploadParams = new ImageUploadParams
+                {
+                    File = new FileDescription(file.FileName, stream),
+                    Transformation = new Transformation().Height(500).Width(500).Crop("fill")
+                };
+
+                uploadResult = await cloudinary.UploadAsync(uploadParams);
+            }
+            return uploadResult;
+        }
+
+        public Task<DeletionResult> DeletePhotoAsync(string publicId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
